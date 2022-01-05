b8fbcf2b071843991783a4ce621b6fcbaeda2785
Tolga Karasay <tolgakarasay@gmail.com>
2022-01-05 20:19:57 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-const fn = function () {
-  console.log('Function is working properly.');
+// Express sayesinde HTTP metodlarını kolaylıkla kullanabiliriz.
+const express = require('express');
+const app = express();
+
+// path modülünü import edelim. Path bir core modül olduğu için download gerekmeden çağırınca gelir.
+const path = require('path');
+
+const myLogger = (req, res, next) => {
+  console.log('Middleware log 1');
+  // next() yazalım ki bir sonraki middleware e ilerleyebilsin. Sayfa yüklenmesi yarıda kalmasın.
+  next();
 };
+
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware log 2');
+  next();
+};
+
+// MIDDLEWARE'ler
+
+// To serve static files such as images, CSS files, and JavaScript files,
+// use the express.static built-in middleware function in Express.
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
+// get request de aslında bir middleware'dir.
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+// Sunucuyu başlatalım
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
+
+// not: request ve response arasında bulunan herşey middleware'dir. Routing bile middleware'dir.
